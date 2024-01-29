dcaa1ab7996e0f2be7b21b24f16c15853f214042
mugambi12 <silasmungiria16@gmail.com>
2024-01-29 17:08:52 +0300

    records database

--- app/backend/auth/forms.py
+++ app/backend/auth/forms.py
@@ -13,11 +13,12 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired(), Length(max=50)])
     last_name = StringField('Last Name', validators=[DataRequired(), Length(max=50)])
-    mobile_number = StringField('Mobile Number', validators=[DataRequired(), Length(min=10, max=20)])
+    mobile_number = StringField('Mobile Number', validators=[DataRequired(), Length(min=9, max=20)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8), EqualTo('confirm_password', message='Passwords must match')])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
     def validate_mobile_number(self, field):
-        if User.query.filter_by(mobile_number=field.data).first():
+        mobile_number = field.data[-9:]
+        if User.query.filter_by(mobile_number=mobile_number).first():
             raise ValidationError('Mobile number is already registered.')
--- app/backend/models/user.py
+++ app/backend/models/user.py
@@ -16,6 +16,9 @@ class User(db.Model, UserMixin):
     is_active = db.Column(db.Boolean, default=True)
     is_admin = db.Column(db.Boolean, default=False)
 
+    # Establishing a one-to-many relationship with records
+    records = db.relationship('Record', backref='user', lazy=True)
+
     def __init__(self, mobile_number, password, first_name=None, last_name=None,
                  email=None, house_section=None, house_number=None, profile_image=None):
         self.mobile_number = mobile_number
@@ -29,3 +32,16 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    meter_reading = db.Column(db.Float)
+    billing_info = db.Column(db.String(255))
+    payment_info = db.Column(db.String(255))
+
+    def __init__(self, meter_reading, billing_info, payment_info, user_id):
+        self.user_id = user_id
+        self.meter_reading = meter_reading
+        self.billing_info = billing_info
+        self.payment_info = payment_info
