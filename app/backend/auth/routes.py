f49ae0b9fcc82046247bcf60fa946abdd980d8ed
mugambi12 <silasmungiria16@gmail.com>
2024-03-07 17:41:07 +0300

    Add password reset functionality

--- app/backend/auth/routes.py
+++ app/backend/auth/routes.py
@@ -62,7 +62,7 @@ def register_apogen_admin():
 
 # Update forms.py
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class PasswordResetForm(FlaskForm):
@@ -72,6 +72,7 @@ class PasswordResetForm(FlaskForm):
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('New Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    token = HiddenField()  # Add a hidden field for the token
     submit = SubmitField('Reset Password')
 
 
@@ -113,40 +114,23 @@ def reset_password_request():
     form = PasswordResetForm()
 
     if form.validate_on_submit():
-        msg = Message("Hey", sender='noreply@apogen.com', recipients=[form.email.data])
-        msg.body = "Hey how are you doing today. This is Apogen Software Solutions"
+        user = User.query.filter_by(email=form.email.data).first()
 
-        mail.send(msg)
-        return "Message sent successfully"
+        if user:
+            token = generate_token(form.email.data)
+            reset_token = PasswordResetToken(email=form.email.data, token=token)
+            db.session.add(reset_token)
+            db.session.commit()
+            if send_password_reset_email(form.email.data, token):
+                flash('An email with instructions to reset your password has been sent.', 'info')
+            else:
+                flash('Failed to send password reset email. Please try again later.', 'danger')
+        else:
+            flash('Email not found in our users.', 'warning')
+
+        return redirect(url_for('auth.login'))
 
     return render_template('auth/reset_password_request.html', form=form, hide_navbar=True, hide_sidebar=True, hide_footer=True)
-
-
-#@auth_bp.route('/reset_password_request', methods=['GET', 'POST'])
-#def reset_password_request():
-#    form = PasswordResetForm()
-#
-#    if form.validate_on_submit():
-#        # Check if the user with the provided email exists
-#        user = User.query.filter_by(email=form.email.data).first()
-#
-#        if user:
-#            # Generate token and save it
-#            token = generate_token(form.email.data)
-#            reset_token = PasswordResetToken(email=form.email.data, token=token)
-#            db.session.add(reset_token)
-#            db.session.commit()
-#            # Send email with password reset instructions
-#            if send_password_reset_email(form.email.data, token):
-#                flash('An email with instructions to reset your password has been sent.', 'info')
-#            else:
-#                flash('Failed to send password reset email. Please try again later.', 'danger')
-#        else:
-#            flash('Email not found in our users.', 'info')
-#
-#        return redirect(url_for('auth.login'))
-#
-#    return render_template('auth/reset_password_request.html', form=form, hide_navbar=True, hide_sidebar=True, hide_footer=True)
 
 @auth_bp.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
@@ -169,4 +153,26 @@ def reset_password(token):
             flash('Invalid user or expired token.', 'danger')
             return redirect(url_for('auth.reset_password_request'))
 
-    return render_template('auth/reset_password.html', form=form, hide_navbar=True, hide_sidebar=True, hide_footer=True)
+    # Pass the token to the template
+    form.token.data = token
+    return render_template('auth/reset_password.html', form=form, token=token, hide_navbar=True, hide_sidebar=True, hide_footer=True), 204
+
+
+
+
+
+
+
+
+#@auth_bp.route('/send_mail_to_user', methods=['GET', 'POST'])
+#def send_mail_to_user():
+#    form = PasswordResetForm()
+#
+#    if form.validate_on_submit():
+#        msg = Message("Hey", sender='noreply@apogen.com', recipients=[form.email.data])
+#        msg.body = "Hey how are you doing today. This is Apogen Software Solutions"
+#
+#        mail.send(msg)
+#        return "Message sent successfully"
+#
+#    return render_template('auth/send_mail_to_user.html', form=form, hide_navbar=True, hide_sidebar=True, hide_footer=True)
