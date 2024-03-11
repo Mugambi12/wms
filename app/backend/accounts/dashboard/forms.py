3b1fdeb95afd32ac9b40705adc0934e24f6b27c6
mugambi12 <silasmungiria16@gmail.com>
2024-03-11 22:45:51 +0300

    Delete unused files and code

--- app/backend/accounts/dashboard/forms.py
+++
@@ -1,8 +0,0 @@
-# File: app/backend/accounts/dashboard/forms.py
-
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-
-class StickyNoteForm(FlaskForm):
-    content = IntegerField('Sticky Note')
-    submit = SubmitField('Submit')
