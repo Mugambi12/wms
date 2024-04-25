decf0f74b07998b0fe4134447c3473b7147abfaa
mugambi12 <silasmungiria16@gmail.com>
2024-04-25 23:32:17 +0300

    Update database setup script to grant PROCESS privilege to 'foo' user

--- wms/core/backend/database/setup/setup.sql
+++ wms/core/backend/database/setup/setup.sql
@@ -5,13 +5,15 @@
 -- Date: February 2024
 
 -- Create the database if it doesn't exist
+-- DROP DATABASE IF EXISTS watermanagement;
 CREATE DATABASE IF NOT EXISTS watermanagement;
 
 -- Create the user if it doesn't exist and grant privileges
 CREATE USER IF NOT EXISTS 'foo'@'localhost' IDENTIFIED BY 'foo123';
 
 -- Ensure that the user has all privileges on the watermanagement database
-GRANT ALL PRIVILEGES ON watermanagement.* TO 'foo'@'localhost';
+GRANT PROCESS ON *.* TO 'foo'@'localhost';
+-- GRANT ALL PRIVILEGES ON watermanagement.* TO 'foo'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
